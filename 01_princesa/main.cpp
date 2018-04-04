fc27146cb15d3042079974ecdbb2a3effcbd6146
RonierLima <ronier.lim@gmail.com>
2018-04-04 11:32:58 -0300

    Up

--- 01_princesa/main.cpp
+++ 01_princesa/main.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-void rodar(int pessoas[], int& qtdPessoas, int& posseArma){
+void rodar(int pessoas[], int& qtdPessoas){
 
-    for(int r = 0; r < posseArma; r++){
+
         int aux = pessoas[0];
 
         for(int i = 0; i < qtdPessoas - 1; i++ )
             pessoas[i] = pessoas[i + 1];
 
         pessoas[qtdPessoas - 1] = aux;
-    }
+
 }
 
 void matar(int pessoas[], int& qtdPessoas){
@@ -34,8 +34,6 @@ int main()
 {
 
     int qtdPessoas, posseArma;
-    int i;
-
 
     cin >> qtdPessoas;
     cin >> posseArma;
@@ -43,19 +41,21 @@ int main()
     int pessoas[qtdPessoas];
 
 
-    for(i = 0; i < qtdPessoas ; i++ )
+    for(int i = 0; i < qtdPessoas ; i++ )
         pessoas[i] = i + 1;
 
     while(pessoas[0] != posseArma){
 
-        rodar(pessoas, qtdPessoas, posseArma);
+        rodar(pessoas, qtdPessoas);
     }
 
     while(qtdPessoas > 1){
 
         mostrar(pessoas, qtdPessoas);
         matar(pessoas, qtdPessoas);
-        rodar(pessoas, qtdPessoas, posseArma);
+        for(int r = 0; r < posseArma; r++){
+            rodar(pessoas, qtdPessoas);
+        }
         posseArma = pessoas[0];
 
     }
